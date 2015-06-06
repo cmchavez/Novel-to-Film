b08baa5651f17bbb5463dc05d871a9ff5b1f312a
Cristal Chavez <cristalmchavez@yahoo.com>
2015-06-05 17:54:36 -0700

    linked novel list item to novel description page, no description available yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604164823) do
+ActiveRecord::Schema.define(version: 20150605233544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "films", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +38,12 @@ ActiveRecord::Schema.define(version: 20150604164823) do
 
   add_index "novels", ["genre_id"], name: "index_novels_on_genre_id", using: :btree
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
